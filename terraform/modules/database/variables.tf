0e43e01759ecc229e559fe674a8d30b463471408
Adrina Colaco <adcola13@gmail.com>
2025-11-05 19:35:19 +0530

    Change default MySQL server location to Central India

--- terraform/modules/database/variables.tf
+++ terraform/modules/database/variables.tf
@@ -1,7 +1,7 @@
 variable "location" {
   description = "Region for the MySQL Flexible Server"
   type        = string
-  default     = "East US" # pick a supported region for your subscription
+  default     = "Central India" # pick a supported region for your subscription
 }
 variable "mysql_admin_username" {}
 variable "mysql_admin_password" {
